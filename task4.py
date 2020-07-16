c612372a54d0455ca2a6ae1186402dcfd770c32f
erldas <daserlikov@gmail.com>
2020-07-16 20:25:32 +0600

    task4

---
+++ task4.py
@@ -0,0 +1,9 @@
+Gosti= ['Yana', 'Tanya', 'Yuliya']
+a= Gosti.pop()
+print(a)
+Gosti.append('Masha')
+print(Gosti)
+b= ('Приходите к 20-00, ')
+print(b+Gosti[0])
+print(b+Gosti[1])
+print(b+Gosti[2])
